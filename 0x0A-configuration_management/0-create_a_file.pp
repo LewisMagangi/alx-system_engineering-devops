27619991c28ab160171e557d61fe7ecf67ba6440
Liquelaliqour <lewismomanyi34@gmail.com>
2024-02-05 06:10:33 -0800

    creating puppet manifests.

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,10 @@
+# A puppet manifest to create a file named 0-create_a_file.pp
+
+file { '/tmp/school':
+  ensure  => file,
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+	  }
+	  
