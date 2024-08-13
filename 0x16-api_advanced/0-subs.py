ca15f462ee8e8689fe3472f012718eb869e78df3
Liquelaliqour <lewismomanyi34@gmail.com>
2024-08-13 10:51:26 -0700

    function that queries the Reddit API and returns the number of subscribers (not active users, total subscribers) for a given subreddit. If an invalid subreddit is given, the function should return 0. Fifth Trial

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-'''
-A function that queries the Reddit API and returns the number of subscribers
+"""
+Function that queries the Reddit API and returns the number of subscribers
 (not active users, total subscribers) for a given subreddit.
-If an invalid subreddit is given, the function should return 0.
-'''
+If an invalid subreddit is given, the function should return 0
+"""
+
 import requests
 
 
@@ -12,15 +13,12 @@ def number_of_subscribers(subreddit):
     Function that queries the Reddit API
     - If not a valid subreddit, return 0.
     """
-    # url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    # headers = {'User-Agent': 'Custom'}
-    response = requests.get(
+    req = requests.get(
         "https://www.reddit.com/r/{}/about.json".format(subreddit),
-        headers={"User-Agent": "Mozilla/5.0"},
-        )
-    if response.status_code == 200:
-        # data = response.json()
-        return response.json().get("data", {}).get("subscribers", 0)
-        # print(data)
+        headers={"User-Agent": "Custom"},
+    )
+
+    if req.status_code == 200:
+        return req.json().get("data").get("subscribers")
     else:
         return 0
