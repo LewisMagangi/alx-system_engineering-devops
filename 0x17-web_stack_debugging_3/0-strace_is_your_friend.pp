563e823775deaee3944484de68cbdf7fab780f06
Liquelaliqour <lewismomanyi34@gmail.com>
2024-08-14 11:26:27 -0700

    A puppet script that  find out why Apache is returning a 500 error

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# A puppet script to fix an Apache server returning a 500 status error code
+
+exec { 'fix error':
+  provider => 'shell',
+  command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php'
+}
